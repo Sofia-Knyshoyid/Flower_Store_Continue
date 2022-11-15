f3d47864720795007838ef32c2c72559aef617b2
Sofia-Knyshoyid <87498412+Sofia-Knyshoyid@users.noreply.github.com>
2022-11-15 10:34:33 +0200

    Update NavigateDelivery.java

--- demo/src/main/java/com/example/demo/delivery/NavigateDelivery.java
+++ demo/src/main/java/com/example/demo/delivery/NavigateDelivery.java
@@ -1,15 +1,17 @@
 package com.example.demo.delivery;
 
+import lombok.Getter;
 import java.util.LinkedList;
 
 public class NavigateDelivery {
+    @Getter
     private Delivery delivery;
 
     public void setDeliveryMethod(Delivery delivery){
         this.delivery = delivery;
     }
 
-    public void PayWithChosenMethod(LinkedList items){
+    public void DeliverWithChosenMethod(LinkedList items){
         delivery.deliver(items);
     }
 
